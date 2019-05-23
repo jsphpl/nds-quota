a8bcedc79cc45ca3e6bafece512ce488dece51ca
Joseph Paul <mail@jsph.pl>
2019-05-23 13:10:08 +0200

    Handle browser back navigation

--- preauth.py
+++ preauth.py
@@ -21,11 +21,24 @@ class PreAuth(BaseApp):
         query = self.parse_query()
         Log.info('Preauth with query string "%s", parsed: %s' % (self.args.query_string, query))
 
+        # User is authenticated but used the back button in their browser
+        if 'status' in query and query['status'] == 'authenticated':
+            placeholders = {}
+            client = self.obtain_client_data(query['clientip'])
+            if client is not None:
+                user = self.find_user_by_mac(client.mac)
+                if user is not None:
+                    placeholders['remaining_quota'] = self.get_remaining_quota(user, client)
+
+            print(self.renderer.render('authenticated', placeholders))
+            exit(0)
+
+
         if ('username' in query) and ('password' in query):
             user = self.find_user(query['username'])
 
             # Bad username
-            if user == False:
+            if user is None:
                 print(self.renderer.render('login', {
                     'query': query,
                     'message': 'User not found. Please check the username.'
@@ -61,10 +74,10 @@ class PreAuth(BaseApp):
     def parse_query(self):
         try:
             unquoted = urllib.parse.unquote(self.args.query_string[3:])
-            splitted = unquoted.split(', ')
+            splitted = unquoted.replace('&', ', ').split(', ')
             pairs = [i.split('=') for i in splitted]
             return dict(pairs)
-        except Exception:
+        except Exception as e:
             return {}
 
     def find_user(self, username):
@@ -72,7 +85,7 @@ class PreAuth(BaseApp):
             return self.models.User.get(username=username)
         except Exception:
             Log.info('Auth attempt with username="%s" failed. User not found.' % username)
-            return False
+            return None
 
     def assign_device_to_user(self, user, mac_address):
         try:
@@ -82,31 +95,42 @@ class PreAuth(BaseApp):
             device = self.models.Device.create(mac_address=mac_address, user_id=user.id)
 
     def obtain_client_data(self, identifier):
-        command = subprocess.Popen(
-            ['ndsctl', 'json', identifier],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-        )
-        stdout, stderr = command.communicate()
-#         stdout = """{
-# "id":1,
-# "ip":"192.168.1.208",
-# "mac":"f8:1a:67:0c:95:a0",
-# "added":0,
-# "active":1558604418,
-# "duration":0,
-# "token":"381149ab",
-# "state":"Preauthenticated",
-# "downloaded":10240,
-# "avg_down_speed":0.00,
-# "uploaded":0,
-# "avg_up_speed":0.00
-# }"""
-        return json.loads(stdout)
+        try:
+            command = subprocess.Popen(
+                ['ndsctl', 'json', identifier],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT
+            )
+            stdout, stderr = command.communicate()
+    #         stdout = """{
+    # "id":1,
+    # "ip":"192.168.1.208",
+    # "mac":"f8:1a:67:0c:95:a0",
+    # "added":0,
+    # "active":1558604418,
+    # "duration":0,
+    # "token":"381149ab",
+    # "state":"Preauthenticated",
+    # "downloaded":10240,
+    # "avg_down_speed":0.00,
+    # "uploaded":0,
+    # "avg_up_speed":0.00
+    # }"""
+            return json.loads(stdout)
+        except Exception:
+            return None
 
     def get_remaining_quota(self, user, client):
         used = (client['downloaded'] + client['uploaded']) * 1024
         return user.max_bytes - used
+
+    def find_user_by_mac(self, mac_address):
+        try:
+            device = self.models.Device.get(mac_address=mac_address)
+            return self.models.User.get(id=device.user_id)
+        except Exception:
+            return None
+
 
 
 if __name__ == '__main__':
